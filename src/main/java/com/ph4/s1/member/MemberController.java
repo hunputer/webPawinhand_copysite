bea05573d2011ef28bc2137dc85723a71ef547e2
hunputer <52592192+hunputer@users.noreply.github.com>
2020-11-30 12:27:58 +0900

    1

--- src/main/java/com/ph4/s1/member/MemberController.java
+++ src/main/java/com/ph4/s1/member/MemberController.java
@@ -266,4 +266,11 @@ public class MemberController {
 		return mv;
 	}
 	
+	@GetMapping("memberJoinForm_shelter")
+	   public ModelAndView setMemberJoinForm_shelter()throws Exception{
+	      ModelAndView mv = new ModelAndView();
+	      mv.setViewName("member/memberJoinForm_shelter");
+	      return mv;
+	  }
+	
 }
